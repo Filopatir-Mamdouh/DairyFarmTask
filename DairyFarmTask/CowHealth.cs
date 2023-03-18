e181f77b75f6e01637a57af51b3ec93d01d5419b
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-18 14:11:38 +0200

    copypasted the previous code to another page

--- DairyFarmTask/CowHealth.cs
+++ DairyFarmTask/CowHealth.cs
@@ -26,5 +26,40 @@ namespace DairyFarmTask
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Milk_Production milk = new Milk_Production();
+            this.Hide();
+            milk.Show();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            Breeding breeding = new Breeding();
+            this.Hide();
+            breeding.Show();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            MilkSales sales = new MilkSales();
+            this.Hide();
+            sales.Show();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            Finance finance = new Finance();
+            this.Hide();
+            finance.Show();
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            this.Hide();
+            dashboard.Show();
+        }
     }
 }
