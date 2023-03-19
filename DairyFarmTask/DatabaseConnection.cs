b03cfbffa1dd98ac9820b6e442eaef6aad826515
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-19 10:47:21 +0200

    Adding connection to the class

--- DairyFarmTask/DatabaseConnection.cs
+++ DairyFarmTask/DatabaseConnection.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,6 @@ namespace DairyFarmTask
 {
     internal class DatabaseConnection
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nour\Documents\DairyFarmDb.mdf;Integrated Security=True;Connect Timeout=30");
     }
 }
