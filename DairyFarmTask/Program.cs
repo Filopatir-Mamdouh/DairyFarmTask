86c041fe24cab6b44e28ce337adf4b32dc589404
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-23 10:30:45 +0200

    changed the startup screen to the main form

--- DairyFarmTask/Program.cs
+++ DairyFarmTask/Program.cs
@@ -16,7 +16,7 @@ namespace DairyFarmTask
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Finance());
+            Application.Run(new Splash());
         }
     }
 }
