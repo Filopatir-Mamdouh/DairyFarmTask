568ea9e6d1d2a494827161b8fb9b02c4de05014b
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-23 06:29:38 +0200

    Changed The Startup Form to test

--- DairyFarmTask/Program.cs
+++ DairyFarmTask/Program.cs
@@ -16,7 +16,7 @@ namespace DairyFarmTask
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Breeding());
+            Application.Run(new Employee());
         }
     }
 }
