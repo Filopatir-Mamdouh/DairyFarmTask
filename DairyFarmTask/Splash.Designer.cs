7e8bed31b9b6a3200a6945110838c1c82ccfeca6
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-23 10:39:14 +0200

    Finished Splash Form

--- DairyFarmTask/Splash.Designer.cs
+++ DairyFarmTask/Splash.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.bunifuProgressBar1 = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.Progress = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,31 +50,36 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Gabriola", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(211, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(355, 88);
             this.label1.TabIndex = 1;
             this.label1.Text = "Dairy Farm Software";
             // 
-            // bunifuProgressBar1
+            // Progress
             // 
-            this.bunifuProgressBar1.BackColor = System.Drawing.Color.Silver;
-            this.bunifuProgressBar1.BorderRadius = 5;
-            this.bunifuProgressBar1.Location = new System.Drawing.Point(4, 380);
-            this.bunifuProgressBar1.MaximumValue = 100;
-            this.bunifuProgressBar1.Name = "bunifuProgressBar1";
-            this.bunifuProgressBar1.ProgressColor = System.Drawing.Color.Teal;
-            this.bunifuProgressBar1.Size = new System.Drawing.Size(793, 10);
-            this.bunifuProgressBar1.TabIndex = 2;
-            this.bunifuProgressBar1.Value = 0;
+            this.Progress.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.Progress.BorderRadius = 5;
+            this.Progress.Location = new System.Drawing.Point(4, 380);
+            this.Progress.MaximumValue = 100;
+            this.Progress.Name = "Progress";
+            this.Progress.ProgressColor = System.Drawing.Color.White;
+            this.Progress.Size = new System.Drawing.Size(793, 10);
+            this.Progress.TabIndex = 2;
+            this.Progress.Value = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(800, 387);
-            this.Controls.Add(this.bunifuProgressBar1);
+            this.Controls.Add(this.Progress);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -89,7 +96,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private Bunifu.Framework.UI.BunifuProgressBar bunifuProgressBar1;
+        private Bunifu.Framework.UI.BunifuProgressBar Progress;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- DairyFarmTask/Splash.cs
+++ DairyFarmTask/Splash.cs
@@ -15,6 +15,21 @@ namespace DairyFarmTask
         public Splash()
         {
             InitializeComponent();
+            timer1.Start();
+        }
+
+        int start = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            start += 1;
+            Progress.Value = start;
+            if (Progress.Value == 100 ) 
+            {
+                timer1.Stop();
+                Login Log = new Login();
+                this.Hide();
+                Log.Show();
+            }
         }
     }
 }
