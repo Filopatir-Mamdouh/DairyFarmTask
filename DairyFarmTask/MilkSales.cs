ec177f1526f87b7910d224e5910fc0e1cbc84eba
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-03-23 07:20:03 +0200

    Added Clear Function

--- DairyFarmTask/MilkSales.cs
+++ DairyFarmTask/MilkSales.cs
@@ -134,5 +134,10 @@ namespace DairyFarmTask
                 }
             }
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
     }
 }
